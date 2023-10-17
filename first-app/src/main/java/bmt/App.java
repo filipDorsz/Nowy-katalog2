5942b625e85771ea33dd3b7ad06de8cd027e694e
filipDorsz <filip.dorsz.lo1.leszno@gmail.com>
2023-10-17 20:48:35 +0200

    test

--- first-app/src/main/java/bmt/App.java
+++ first-app/src/main/java/bmt/App.java
@@ -1,13 +1,40 @@
 package bmt;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import java.util.Scanner;
+
+public class App {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("Podaj imię: ");
+    String name = scanner.nextLine();
+
+    System.out.print("Podaj wiek: ");
+    int age = scanner.nextInt();
+
+    scanner.nextLine(); // Odczyt zbędnej nowej linii
+
+    System.out.print("Podaj ulicę: ");
+    String street = scanner.nextLine();
+
+    System.out.print("Podaj miasto: ");
+    String city = scanner.nextLine();
+
+    System.out.print("Podaj kod pocztowy: ");
+    String postalCode = scanner.nextLine();
+
+    Person person = new Person(name, age);
+    Address address = new Address(street, city, postalCode);
+
+    System.out.println("Dane osoby:");
+    System.out.println("Imię: " + person.getName());
+    System.out.println("Wiek: " + person.getAge());
+
+    System.out.println("Adres:");
+    System.out.println("Ulica: " + address.getStreet());
+    System.out.println("Miasto: " + address.getCity());
+    System.out.println("Kod pocztowy: " + address.getPostalCode());
+
+    scanner.close();
+  }
 }
